260bc3ef9a18639d5bc8617fd4c57ba1142a7fb3
web58 <sait-darya@mail.ru>
2020-02-17 17:56:04 +0300

    1 экран

--- src/main.js
+++ src/main.js
@@ -4,9 +4,24 @@ jQuery(function($) {
 
   $(document).ready(function () {
 
-    console.log('YEP');
+    console.log('YEP1');
 
-    
+    var wWindow = $(window).width();
+
+    // burger start
+
+    $('.burger').on('click', function (e) {
+      $('.header__menu').toggleClass('fixed');
+
+      if (wWindow < 768) {
+        $('.header__call').toggleClass('mobile');
+      }
+
+      $('.burger').toggleClass('burger-closed');
+    });
+
+
+    // burger end
 
   });
 
